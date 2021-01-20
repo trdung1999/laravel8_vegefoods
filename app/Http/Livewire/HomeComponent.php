30501d1d437b7258cff3cfc1c6a6eefe00913e5f
TrungDungNguyen <63592403+trdung1999@users.noreply.github.com>
2021-01-20 21:54:44 +0700

    show product dynamic done

--- app/Http/Livewire/HomeComponent.php
+++ app/Http/Livewire/HomeComponent.php
@@ -3,11 +3,14 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Livewire\WithPagination;
+use App\Models\Product;
 class HomeComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.home-component')->layout('layouts.base');
+        $products = Product::paginate(8);
+        return view('livewire.home-component',['products' => $products])->layout('layouts.base');
     }
 }
--- app/Http/Livewire/ShopComponent.php
+++ app/Http/Livewire/ShopComponent.php
@@ -1,13 +1,16 @@
 <?php
 
 namespace App\Http\Livewire;
-
+use App\Models\Product;
+use Livewire\WithPagination;
 use Livewire\Component;
 
 class ShopComponent extends Component
 {
+    use WithPagination;
     public function render()
     {
-        return view('livewire.shop-component')->layout('layouts.base');
+        $products = Product::paginate(12);
+        return view('livewire.shop-component',['products' => $products])->layout('layouts.base');
     }
 }
