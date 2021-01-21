44c73414e3e8e67c6c83e46bf5a45812a6b62963
TrungDungNguyen <63592403+trdung1999@users.noreply.github.com>
2021-01-21 20:44:37 +0700

    route to details product done

--- resources/views/livewire/home-component.blade.php
+++ resources/views/livewire/home-component.blade.php
@@ -141,37 +141,46 @@
   <div class="container">
     <div class="row">
       @foreach ($products as $product)
-      <div class="col-md-6 col-lg-3 ftco-animate">
-        <div class="product">
-          <a href="" class="img-prod"><img class="img-fluid" src="{{ asset('images/') }}/{{ $product->image }}" alt="Colorlib Template">
-            {{-- <span class="status">30%</span> --}}
-            <div class="overlay"></div>
-          </a>
-          <div class="text py-3 pb-4 px-3 text-center">
-            <h3><a href="">{{ $product->name }}</a></h3>
-            <div class="d-flex">
-              <div class="pricing">
-                <p class="price"><span class="mr-2 price-dc"></span><span class="price-sale">{{ '$'.$product->regular_price }}</span></p>
+      <div class="col-md-6 col-lg-3 ftco-animate">                
+          <div class="product">
+              <a href="{{ route('product.details',['slug'=>$product->slug]) }}" class="img-prod"><img class="img-fluid" src="{{ asset('images') }}/{{ $product->image }}" alt="Colorlib Template">
+                @isset($product->sale_price)
+                <span class="status">30%</span>                        
+                @endisset 
+                  <div class="overlay"></div>
+              </a>
+              <div class="text py-3 pb-4 px-3 text-center">
+                  <h3><a href="{{ route('product.details',['slug'=>$product->slug]) }}">{{ $product->name }}</a></h3>
+                  <div class="d-flex">
+                      <div class="pricing">
+                            <p class="price">
+                            @isset($product->sale_price)
+                                <span class="mr-2 price-dc">{{ $product->regular_price }}</span>
+                                <span class="price-sale">{{ $product->sale_price }}</span></p>
+                            @else
+                                <span class="price-sale">{{ $product->regular_price }}</span></p>
+                            @endisset
+                            
+                      </div>
+                  </div> 
+                  <div class="bottom-area d-flex px-3">
+                      <div class="m-auto d-flex">
+                          <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
+                              <span><i class="ion-ios-menu"></i></span>
+                          </a>
+                          <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
+                              <span><i class="ion-ios-cart"></i></span>
+                          </a>
+                          <a href="#" class="heart d-flex justify-content-center align-items-center ">
+                              <span><i class="ion-ios-heart"></i></span>
+                          </a>
+                      </div>
+                  </div>
               </div>
-            </div>
-            <div class="bottom-area d-flex px-3">
-              <div class="m-auto d-flex">
-                <a href="#" class="add-to-cart d-flex justify-content-center align-items-center text-center">
-                  <span><i class="ion-ios-menu"></i></span>
-                </a>
-                <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
-                  <span><i class="ion-ios-cart"></i></span>
-                </a>
-                <a href="#" class="heart d-flex justify-content-center align-items-center ">
-                  <span><i class="ion-ios-heart"></i></span>
-                </a>
-              </div>
-            </div>
           </div>
-        </div>
       </div>
       @endforeach
-    </div>
+  </div>
   </div>
 </section>
 
